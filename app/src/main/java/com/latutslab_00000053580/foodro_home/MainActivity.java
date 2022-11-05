6e759ee27d5342609275c5ef940a549ec255af96
RendyW <rendywt03@gmail.com>
2022-11-05 16:57:38 +0700

    Initial commit

---
+++ app/src/main/java/com/latutslab_00000053580/foodro_home/MainActivity.java
@@ -0,0 +1,14 @@
+package com.latutslab_00000053580.foodro_home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
